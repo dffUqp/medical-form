d8c38074d263c7bad295b7450b794fcbc4cc4844
dffuqp <yuriivasilchuk.work@gmail.com>
2023-05-21 15:17:48 +0300

    feat(*): initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/button-has-type': 0,
+    'react/jsx-one-expression-per-line': 0,
+    '@typescript-eslint/no-shadow': 0,
+    'react/no-array-index-key': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'react/function-component-definition': 0,
+    '@typescript-eslint/no-empty-interface': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: 'src', replacement: '/src' }],
+  },
+});
