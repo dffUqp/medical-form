9493b9af09ed17db125a4dc280473896d6a82612
dffuqp <yuriivasilchuk.work@gmail.com>
2023-05-30 02:17:44 +0300

    feat: hide scrollbar for medicalForm on mobile

--- src/components/CustomScrollContainer.tsx
+++ src/components/CustomScrollContainer.tsx
@@ -1,23 +1,25 @@
 import { Box, styled } from '@mui/material';
 
-const CustomScrollContainer = styled(Box)(() => ({
+const CustomScrollContainer = styled(Box)(({ theme }) => ({
   position: 'relative',
   overflow: 'auto',
   maxWidth: '550px',
   width: '100%',
-  maxHeight: '550px',
-  scrollbarWidth: 'thin',
-  '&::-webkit-scrollbar': {
-    width: '0.4em',
-  },
-  '&::-webkit-scrollbar-track': {
-    background: '#323c41',
-  },
-  '&::-webkit-scrollbar-thumb': {
-    backgroundColor: '#888',
-  },
-  '&::-webkit-scrollbar-thumb:hover': {
-    background: '#555',
+  [theme.breakpoints.up('md')]: {
+    maxHeight: '550px',
+    scrollbarWidth: 'thin',
+    '&::-webkit-scrollbar': {
+      width: '0.4em',
+    },
+    '&::-webkit-scrollbar-track': {
+      background: '#323c41',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: '#888',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      background: '#555',
+    },
   },
 }));
 
